e6f38a3da571c8625ed45084dee3d5db536090a4
samuel <samuelruiz@gmail.com>
2019-08-21 00:11:36 -0700

    Updates

---
+++ precache-manifest.27b2940da65b56455d26ac0673444346.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b956779f17b84d9b460610dc8872d62",
+    "url": "/giphy-clone/index.html"
+  },
+  {
+    "revision": "58a161e57dfa0fbb528d",
+    "url": "/giphy-clone/static/css/main.f8708ff5.chunk.css"
+  },
+  {
+    "revision": "9e500133a828d8ecdd28",
+    "url": "/giphy-clone/static/js/2.dc5fe122.chunk.js"
+  },
+  {
+    "revision": "58a161e57dfa0fbb528d",
+    "url": "/giphy-clone/static/js/main.a75dd544.chunk.js"
+  },
+  {
+    "revision": "c2518c315fd4828176c0",
+    "url": "/giphy-clone/static/js/runtime~main.cbef7cc5.js"
+  },
+  {
+    "revision": "1eacda120135651fd5727e822b64e476",
+    "url": "/giphy-clone/static/media/gifNotFound.1eacda12.gif"
+  }
+]);
